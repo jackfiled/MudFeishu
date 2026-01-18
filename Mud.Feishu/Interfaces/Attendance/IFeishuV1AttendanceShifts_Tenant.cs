403ef8573878f03e1550096c78489ea808d37af5
倔强的石头 <413901125@qq.com>
2026-01-18 22:12:17 +0800

    feat(attendance): 添加获取班次详情接口及相关数据模型
    添加通过班次ID获取班次详情的接口方法，并实现相关请求和响应数据模型，包括休息弹性设置等配置项

--- Mud.Feishu/Interfaces/Attendance/IFeishuV1AttendanceShifts_Tenant.cs
+++ Mud.Feishu/Interfaces/Attendance/IFeishuV1AttendanceShifts_Tenant.cs
@@ -40,4 +40,16 @@ public interface IFeishuTenantV1AttendanceShifts
     Task<FeishuNullDataApiResult?> DeleteShiftsByIdAsync(
        [Path] string shift_id,
        CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// <para>通过班次 ID 获取班次详情。对应功能为假勤设置-班次设置班次列表中的具体班次，班次信息可以点击班次名称查看</para>
+    /// </summary>
+    /// <param name="shift_id">班次 ID，获取方式：1）按名称查询班次 2）创建班次 示例值："6919358778597097404"</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    /// <returns></returns>
+    [Get("/open-apis/attendance/v1/shifts/{shift_id}")]
+    Task<FeishuApiResult<GetAttendanceShiftsResult>?> GetShiftsByIdAsync(
+      [Path] string shift_id,
+      CancellationToken cancellationToken = default);
 }
