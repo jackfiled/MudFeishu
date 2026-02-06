29f5eccaf592b1f793a456ca5faa47e0c107b3b9
倔强的石头 <413901125@qq.com>
2026-02-06 17:59:32 +0800

    test(飞书群公告): 添加测试用例验证群公告相关接口
    添加详细的测试数据以验证飞书群公告接口的请求和响应处理，包括获取公告信息、公告块列表、创建公告块以及删除公告块等功能

--- Tests/Mud.Feishu.Tests/ChatGroup/IFeishuV1ChatGroupAnnouncementTests.cs
+++ Tests/Mud.Feishu.Tests/ChatGroup/IFeishuV1ChatGroupAnnouncementTests.cs
@@ -31,7 +31,24 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_GetNoticeInfoByIdAsync_Result()
     {
-        string resultStr = "";
+        string resultStr = """
+                        {
+                "code": 0,
+                "msg": "success",
+                "data": {
+                    "revision_id": 1,
+                    "create_time": 1609296809,
+                    "update_time": 1609296809,
+                    "owner_id": "ou_7d8a6e6df7621556ce0d21922b676706ccs",
+                    "owner_id_type": "user_id",
+                    "modifier_id": "ou_7d8a6e6df7621556ce0d21922b676706ccs",
+                    "modifier_id_type": "user_id",
+                    "announcement_type": "docx",
+                    "create_time_v2": "1609296809",
+                    "update_time_v2": "1609296809"
+                }
+            }
+            """;
         var result = JsonSerializer.Deserialize<FeishuApiResult<GetAnnouncementResult>>(resultStr, _jsonSerializerOptions);
 
         Assert.NotNull(result);
@@ -44,7 +61,183 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_GetNoticeBlocksListByIdAsync_Result()
     {
-        string resultStr = "";
+        string resultStr = """
+                        {
+                "code": 0,
+                "data": {
+                    "has_more": false,
+                    "items": [
+                        {
+                            "block_id": "oc_5ad11d72b830411d72b836c20",
+                            "block_type": 1,
+                            "children": [
+                                "EJ8pdBT6RoA2BtxKXfuc1TD0n8f",
+                                "WoF4dolv0ohvegxctR9c1t0sn4b",
+                                "UPD7dhkrLo8ZCfxoAYLcuEbDnzd"
+                            ],
+                            "page": {
+                                "elements": [
+                                    {
+                                        "text_run": {
+                                            "content": "",
+                                            "text_element_style": {
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    }
+                                ],
+                                "style": {
+                                    "align": 1
+                                }
+                            },
+                            "parent_id": ""
+                        },
+                        {
+                            "block_id": "EJ8pdBT6RoA2BtxKXfuc1TD0n8f",
+                            "block_type": 3,
+                            "heading1": {
+                                "elements": [
+                                    {
+                                        "text_run": {
+                                            "content": "新版群公告",
+                                            "text_element_style": {
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    }
+                                ],
+                                "style": {
+                                    "align": 1,
+                                    "folded": false
+                                }
+                            },
+                            "parent_id": "oc_5ad11d72b830411d72b836c20"
+                        },
+                        {
+                            "block_id": "WoF4dolv0ohvegxctR9c1t0sn4b",
+                            "block_type": 2,
+                            "parent_id": "oc_5ad11d72b830411d72b836c20",
+                            "text": {
+                                "elements": [
+                                    {
+                                        "text_run": {
+                                            "content": "当你需要向群内所有成员传达",
+                                            "text_element_style": {
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "text_run": {
+                                            "content": "重要信息",
+                                            "text_element_style": {
+                                                "bold": true,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "text_run": {
+                                            "content": "时，可以使用群公告功能。后续入群的成员也可看到当前群公告。",
+                                            "text_element_style": {
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    }
+                                ],
+                                "style": {
+                                    "align": 1,
+                                    "folded": false
+                                }
+                            }
+                        },
+                        {
+                            "block_id": "UPD7dhkrLo8ZCfxoAYLcuEbDnzd",
+                            "block_type": 2,
+                            "parent_id": "oc_5ad11d72b830411d72b836c20",
+                            "text": {
+                                "elements": [
+                                    {
+                                        "text_run": {
+                                            "content": "群公告编辑",
+                                            "text_element_style": {
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "text_run": {
+                                            "content": "支持实时保存及多人协作",
+                                            "text_element_style": {
+                                                "background_color": 4,
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "text_run": {
+                                            "content": "，群成员",
+                                            "text_element_style": {
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "text_run": {
+                                            "content": "可实时查看最新群公告内容。",
+                                            "text_element_style": {
+                                                "background_color": 4,
+                                                "bold": false,
+                                                "inline_code": false,
+                                                "italic": false,
+                                                "strikethrough": false,
+                                                "underline": false
+                                            }
+                                        }
+                                    }
+                                ],
+                                "style": {
+                                    "align": 1,
+                                    "folded": false
+                                }
+                            }
+                        }
+                    ]
+                },
+                "msg": "success"
+            }
+            """;
         var result = JsonSerializer.Deserialize<FeishuApiPageListResult<AnnouncementBlock>>(resultStr, _jsonSerializerOptions);
 
         Assert.NotNull(result);
@@ -58,10 +251,44 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_CreateNoticeBlockAsync_RequestBody()
     {
-        string bodyStr = "";
+        string bodyStr = """
+                        {
+                "index": 0,
+                "children": [
+                    {
+                        "block_type": 2,
+                        "text": {
+                            "elements": [
+                                {
+                                    "text_run": {
+                                        "content": "当你需要向群内所有成员传达重要信息时，可以使用群公告功能。后续入群的成员也可看到当前群公告。",
+                                        "text_element_style": {
+                                            "background_color": 14,
+                                            "text_color": 5
+                                        }
+                                    }
+                                },
+                                {
+                                    "text_run": {
+                                        "content": "群公告编辑支持实时保存及多人协作，群成员可实时查看最新群公告内容。",
+                                        "text_element_style": {
+                                            "background_color": 14,
+                                            "bold": true,
+                                            "text_color": 5
+                                        }
+                                    }
+                                }
+                            ],
+                            "style": {}
+                        }
+                    }
+                ]
+            }
+            """;
         var requestBody = JsonSerializer.Deserialize<CreateBlockRequest>(bodyStr, _jsonSerializerOptions);
 
         Assert.NotNull(requestBody);
+        Assert.NotEmpty(requestBody.Childrens!);
     }
 
     /// <summary>
@@ -70,11 +297,52 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_CreateNoticeBlockAsync_Result()
     {
-        string resultStr = "";
+        string resultStr = """
+                        {
+                "code": 0,
+                "data": {
+                    "children": [
+                        {
+                            "block_id": "doxcnXgNGAtaAraIRVeCfmbx4Eo",
+                            "block_type": 2,
+                            "parent_id": "oc_5ad11d72b830411d72b836c20",
+                            "text": {
+                                "elements": [
+                                    {
+                                        "text_run": {
+                                            "content": "当你需要向群内所有成员传达重要信息时，可以使用群公告功能。后续入群的成员也可看到当前群公告。",
+                                            "text_element_style": {
+                                                "background_color": 14,
+                                                "text_color": 5
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "text_run": {
+                                            "content": "群公告编辑支持实时保存及多人协作，群成员可实时查看最新群公告内容。",
+                                            "text_element_style": {
+                                                "background_color": 14,
+                                                "bold": true,
+                                                "text_color": 5
+                                            }
+                                        }
+                                    }
+                                ],
+                                "style": {}
+                            }
+                        }
+                    ],
+                    "client_token": "ea403093-3af1-4e9d-8f5d-53c5a4e4c36e",
+                    "document_revision_id": 2
+                },
+                "msg": ""
+            }
+            """;
         var result = JsonSerializer.Deserialize<FeishuApiResult<CreateBlockResult>>(resultStr, _jsonSerializerOptions);
 
         Assert.NotNull(result);
         Assert.NotNull(result.Data);
+        Assert.NotNull(result.Data.Childrens);
     }
 
     /// <summary>
@@ -108,7 +376,9 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_GetBlockContentByIdAsync_Result()
     {
-        string resultStr = "";
+        string resultStr = """
+                      
+            """;
         var result = JsonSerializer.Deserialize<FeishuApiResult<GetBlockContentListResult>>(resultStr, _jsonSerializerOptions);
 
         Assert.NotNull(result);
@@ -135,7 +405,12 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_DeleteBlockByIdAsync_RequestBody()
     {
-        string bodyStr = "";
+        string bodyStr = """
+                        {
+              "start_index": 0,
+              "end_index": 1
+            }
+            """;
         var requestBody = JsonSerializer.Deserialize<DeleteAnnouncementBlockRequest>(bodyStr, _jsonSerializerOptions);
 
         Assert.NotNull(requestBody);
@@ -147,10 +422,21 @@ public class IFeishuV1ChatGroupAnnouncementTests
     [Fact]
     public void Test_DeleteBlockByIdAsync_Result()
     {
-        string resultStr = "";
+        string resultStr = """
+                        {
+                "code": 0,
+                "msg": "success",
+                "data": {
+                    "revision_id": 1,
+                    "client_token": "fe599b60-450f-46ff-b2ef-9f6675625b97"
+                }
+            }
+            """;
         var result = JsonSerializer.Deserialize<FeishuApiResult<DeleteAnnouncementBlockResult>>(resultStr, _jsonSerializerOptions);
 
         Assert.NotNull(result);
         Assert.NotNull(result.Data);
+        Assert.NotNull(result.Data.RevisionId);
+        Assert.NotEmpty(result.Data.ClientToken!);
     }
 }
