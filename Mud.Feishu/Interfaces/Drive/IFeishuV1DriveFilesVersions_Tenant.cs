212f1ee34645dc7379908f688a9fbe4b27b1d06a
倔强的石头 <413901125@qq.com>
2026-03-10 22:42:50 +0800

    feat(drive): 添加飞书文件版本管理接口
    新增飞书文件版本管理相关接口定义，包括基础接口、用户访问令牌接口和租户访问令牌接口。这些接口用于支持文件版本的创建、删除和获取操作。同时更新项目文件添加对应的文件夹结构。

---
+++ Mud.Feishu/Interfaces/Drive/IFeishuV1DriveFilesVersions_Tenant.cs
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------
+//  作者：Mud Studio  版权所有 (c) Mud Studio 2025   
+//  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+//  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
+//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+// -----------------------------------------------------------------------
+
+namespace Mud.Feishu;
+
+/// <summary>
+/// 文件版本是基于文件生成的新版本，版本依附于文件而存在。飞书开放平台支持基于在线文档和电子表格创建、删除和获取版本信息。
+/// <para>接口详细文档请参见：<see href="https://open.feishu.cn/document/server-docs/docs/drive-v1/file-version/overview"/></para>
+/// </summary>
+[HttpClientApi(TokenManage = nameof(IFeishuAppManager), RegistryGroupName = "Drive", InheritedFrom = nameof(FeishuV1DriveFilesVersions))]
+[Header(Consts.Authorization)]
+[Token(TokenType.TenantAccessToken)]
+public interface IFeishuTenantV1DriveFilesVersions : IFeishuV1DriveFilesVersions
+{
+}
